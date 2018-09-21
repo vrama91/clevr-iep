Fixes bug in corss entropy/neg log likelihood computation for sequences in original iep code.

iep/models/seq2seq.py
@@ -13,6 +13,7 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 
 from iep.embedding import expand_embedding_vocab
+from iep.misc import sparse_softmax_cross_entropy_with_logits
 
 class Seq2Seq(nn.Module):
   def __init__(self,
@@ -103,21 +104,21 @@ class Seq2Seq(nn.Module):
     rnn_output, (ht, ct) = self.decoder_rnn(rnn_input, (h0, c0))
 
     rnn_output_2d = rnn_output.contiguous().view(N * T_out, H)
-    output_logprobs = self.decoder_linear(rnn_output_2d).view(N, T_out, V_out)
+    output_logits = self.decoder_linear(rnn_output_2d).view(N, T_out, V_out)
 
-    return output_logprobs, ht, ct
+    return output_logits, ht, ct
 
-  def compute_loss(self, output_logprobs, y):
+  def compute_loss(self, output_logits, y, crossent_reduction='mean'):
     """
     Compute loss. We assume that the first element of the output sequence y is
     a start token, and that each element of y is left-aligned and right-padded
-    with self.NULL out to T_out. We want the output_logprobs to predict the
+    with self.NULL out to T_out. We want the output_logits to predict the
     sequence y, shifted by one timestep so that y[0] is fed to the network and
     then y[1] is predicted. We also don't want to compute loss for padded
     timesteps.
 
     Inputs:
-    - output_logprobs: Variable of shape (N, T_out, V_out)
+    - output_logits: Variable of shape (N, T_out, V_out)
     - y: LongTensor Variable of shape (N, T_out)
     """
     self.multinomial_outputs = None
@@ -129,15 +130,47 @@ class Seq2Seq(nn.Module):
     out_mask = Variable(torch.Tensor(N, T_out).fill_(0).type_as(mask))
     out_mask[:, :-1] = mask[:, 1:]
     out_mask = out_mask.view(N, T_out, 1).expand(N, T_out, V_out)
-    out_masked = output_logprobs[out_mask].view(-1, V_out)
-    loss = F.cross_entropy(out_masked, y_masked)
+    out_masked = output_logits[out_mask].view(-1, V_out)
+    loss = sparse_softmax_cross_entropy_with_logits(out_masked, y_masked,
+                                                    reduction=crossent_reduction)
     return loss
 
-  def forward(self, x, y):
+  def compute_loss_V2(self, output_logits, y):
+    """
+    Compute loss. We assume that the first element of the output sequence y is
+    a start token, and that each element of y is left-aligned and right-padded
+    with self.NULL out to T_out. We want the output_logits to predict the
+    sequence y, shifted by one timestep so that y[0] is fed to the network and
+    then y[1] is predicted. We also don't want to compute loss for padded
+    timesteps.
+
+    Inputs:
+    - output_logits: Variable of shape (N, T_out, V_out)
+    - y: LongTensor Variable of shape (N, T_out)
+    """
+    self.multinomial_outputs = None
+    _dims = self.get_dims(y=y)
+    mask = y.data != self.NULL
+
+    mask_use = Variable(mask[:, 1:].float())
+    y_use = y[:, 1:]
+    logits_use = output_logits[:, :-1, :]
+
+    loss = sparse_softmax_cross_entropy_with_logits(logits_use,
+                                                    y_use)
+    return torch.sum(loss*mask_use, dim=1)
+
+  def forward(self, x, y, crossent_reduction='mean'):
     encoded = self.encoder(x)
-    output_logprobs, _, _ = self.decoder(encoded, y)
-    loss = self.compute_loss(output_logprobs, y)
-    return loss
+    output_logits, _, _ = self.decoder(encoded, y)
+    neg_logprobs = self.compute_loss_V2(output_logits, y)
+
+    if crossent_reduction == 'mean':
+      return torch.mean(neg_logprobs)
+    elif crossent_reduction is None:
+      return neg_logprobs
+    else:
+      raise NotImplementedError
 
   def sample(self, x, max_length=50):
     # TODO: Handle sampling for minibatch inputs